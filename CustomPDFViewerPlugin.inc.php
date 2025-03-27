2de9b0598f03975203defb2bcf847a8222e437ef
Jonathan Venegas <jonathan.9129@gmail.com>
2025-03-27 11:36:52 -0500

    Add index.php

---
+++ CustomPDFViewerPlugin.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+use PKP\plugins\GenericPlugin;
+use PKP\template\TemplateManager;
+use PKP\plugins\Hook;
+
+class CustomPDFViewerPlugin extends GenericPlugin {
+    /**
+     * @see Plugin::register()
+     */
+    function register($category, $path, $mainContextId = null) {
+        $success = parent::register($category, $path, $mainContextId);
+        if (!$success) return false;
+
+        if ($this->getEnabled()) {
+            Hook::add('TemplateManager::display', [$this, 'addPdfButton']);
+        }
+        return true;
+    }
+
+    /**
+     * Get the display name of this plugin.
+     * @return string
+     */
+    function getDisplayName() {
+        return __('plugins.generic.CustomPDFViewer.displayName');
+    }
+
+    /**
+     * Get the description of this plugin.
+     * @return string
+     */
+    function getDescription() {
+        return __('plugins.generic.CustomPDFViewer.description');
+    }
+
+    /**
+     * Adds a button for PDFs in the galley section.
+     */
+    function addPdfButton($hookName, $args) {
+        $templateMgr = $args[0];
+        $template = $args[1];
+
+        if ($template === 'frontend/pages/article.tpl') {
+            $article = $templateMgr->getTemplateVars('article');
+            $galleys = $article->getGalleys();
+            
+            foreach ($galleys as $galley) {
+                if ($galley->getFileType() === 'application/pdf') {
+                    $pdfUrl = $galley->getRemoteURL() ?: $galley->getLocalizedFileUrl();
+                    $buttonHtml = '<a href="' . $pdfUrl . '" class="btn btn-primary" target="_blank">' . __('plugins.generic.CustomPDFViewer.viewPdf') . '</a>';
+                    $templateMgr->assign('pdfButton', $buttonHtml);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Required for registering the plugin
+require_once('CustomPDFViewerPlugin.inc.php');
+
+// Return the plugin instance
+return new CustomPDFViewerPlugin();
